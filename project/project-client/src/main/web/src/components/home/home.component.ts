965469b14010ffb636022e35fe2969c3bed23090
viktorkulygin <Adolf1488)>
2021-04-05 17:04:11 +0300

    project implemented structure

---
+++ project/project-client/src/main/web/src/components/home/home.component.ts
@@ -0,0 +1,21 @@
+﻿import {Component, OnInit} from '@angular/core';
+import {SharedService} from "../../services/shared.service";
+
+
+@Component({
+    selector: 'home',
+    templateUrl: './home.component.html',
+    styleUrls: [
+        './home.component.css'
+    ]
+})
+
+export class HomeComponent implements OnInit {
+
+    constructor(private shared: SharedService) {
+    }
+
+    ngOnInit() {
+
+    }
+}
