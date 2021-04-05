de57bd62ec83a38cc98ca142cbc60a06517a4bb1
viktorkulygin <Adolf1488)>
2021-04-05 19:10:09 +0300

    project designed app header and added lang changing

--- project/project-client/src/main/web/src/app.component.ts
+++ project/project-client/src/main/web/src/app.component.ts
@@ -1,4 +1,6 @@
-﻿import {Component} from '@angular/core';
+﻿import {Component, OnInit} from '@angular/core';
+import {TranslateService} from "@ngx-translate/core";
+import {Router} from "@angular/router";
 
 @Component({
     selector: 'app-root',
@@ -8,8 +10,45 @@
     ]
 })
 
-export class AppComponent {
+export class AppComponent implements OnInit {
 
-    constructor() {
+    constructor(private translateService: TranslateService,
+                private router: Router) {
+    }
+
+    ngOnInit(): void {
+        if (!this.existsDefaultLanguage()) {
+            this.setEnLang();
+        } else {
+            this.setLang(this.getCurrentLang());
+        }
+    }
+
+    public setRuLang(): void {
+        this.setCurrentLang('ru');
+        this.translateService.setDefaultLang('ru');
+        this.router.navigateByUrl(this.router.url.replace('en', 'ru'));
+    }
+
+    public setEnLang(): void {
+        this.setCurrentLang('en');
+        this.translateService.setDefaultLang('en');
+        this.router.navigateByUrl(this.router.url.replace('ru', 'en'));
+    }
+
+    public setLang(lang: string) {
+        this.translateService.setDefaultLang(lang);
+    }
+
+    public setCurrentLang(lang: string) {
+        localStorage.setItem('lang', lang);
+    }
+
+    public getCurrentLang() {
+        return localStorage.getItem('lang');
+    }
+
+    public existsDefaultLanguage() {
+        return localStorage.getItem('lang') !== null;
     }
 }
