99a4fa6286e77f4135957e9079a36913abe1e780
viktorkulygin <Adolf1488)>
2021-04-07 18:15:33 +0300

    project added client pages structure, added pages for login and sign-up, also configured routing

--- project/project-client/src/main/web/src/app.routing.ts
+++ project/project-client/src/main/web/src/app.routing.ts
@@ -1,9 +1,22 @@
 ﻿import {RouterModule, Routes} from '@angular/router';
 import {HomeComponent} from './components/home/home.component';
+import {CandidatesComponent} from "./components/candidadtes/candidates.component";
+import {InterviewersComponent} from "./components/interviewers/interviewers.component";
+import {InterviewsComponent} from "./components/interviews/interviews.component";
+import {LoginComponent} from "./components/login/login.component";
+import {SignUpComponent} from "./components/sign-up/sign-up.component";
+import {TemplatesComponent} from "./components/templates/templates.component";
 
 const appRoutes: Routes = [
-    { path: ':lang/home', component: HomeComponent },
-    { path: '**', redirectTo: 'en/home' }
+    {path: ':lang/home', component: HomeComponent},
+    {path: ':lang/candidates', component: CandidatesComponent},
+    {path: ':lang/interviewers', component: InterviewersComponent},
+    {path: ':lang/interviews', component: InterviewsComponent},
+    {path: ':lang/templates', component: TemplatesComponent},
+    {path: ':lang/template-criteria', component: TemplatesComponent},
+    {path: ':lang/login', component: LoginComponent},
+    {path: ':lang/sign-up', component: SignUpComponent},
+    {path: '**', redirectTo: 'en/home'}
 ];
 
 export const routing = RouterModule.forRoot(appRoutes,
---
+++ project/project-client/src/main/web/src/components/template-criteria/template-criteria.component.ts
@@ -0,0 +1,19 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+    selector: 'template-criteria',
+    templateUrl: './template-criteria.component.html',
+    styleUrls: [
+        './template-criteria.component.css'
+    ]
+})
+
+export class TemplateCriteriaComponent implements OnInit {
+
+    constructor() {
+    }
+
+    ngOnInit(): void {
+
+    }
+}
