41fc6d05edc4568d935c2586a02443768de78c28
viktorkulygin <Adolf1488)>
2021-04-25 22:28:52 +0300

    ht10 removed unused

--- ht10-spring-mvc-spa/ht10-server/src/main/java/ru/otus/kulygin/service/UiService.java
+++
@@ -1,8 +0,0 @@
-package ru.otus.kulygin.service;
-
-public interface UiService {
-
-    String getLocalizedMessageForUser(String key, String ...args);
-    String getObjectForPrettyPrint(Object object);
-
-}
